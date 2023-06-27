dbe04a83335da7915c377505f083003e34ab8886
Pabl0Romano <romae047@gmail.com>
2023-06-26 23:17:15 -0300

    timeline

--- script.js
+++ script.js
@@ -26,6 +26,76 @@ window.addEventListener("load", () => {
     let html = document.querySelector(".html");
     let css = document.querySelector(".css");
     let react = document.querySelector(".react");
+    let dot1 = document.querySelector(".dot1")
+    let dot2 = document.querySelector(".dot2")
+    let dot3 = document.querySelector(".dot3")
+    let dot4 = document.querySelector(".dot4")
+    let dot5 = document.querySelector(".dot5")
+    let tdot1 = document.querySelector(".dot-text1")
+    let tdot2 = document.querySelector(".dot-text2")
+    let tdot3 = document.querySelector(".dot-text3")
+    let tdot4 = document.querySelector(".dot-text4")
+    let tdot5 = document.querySelector(".dot-text5")
+
+    dot1.addEventListener("mouseover", ()=> {
+        dot1.style.width = "1.5rem";
+        dot1.style.borderStyle = "solid";
+        dot1.style.borderColor = "white"
+        tdot1.style.opacity = 100;
+    });
+    dot1.addEventListener("mouseout", ()=> {
+        dot1.style.width = "1rem";
+        dot1.style.borderStyle = "none";
+        tdot1.style.opacity = 0;
+    });
+
+    dot2.addEventListener("mouseover", ()=> {
+        dot2.style.width = "1.5rem";
+        dot2.style.borderStyle = "solid";
+        dot2.style.borderColor = "white"
+        tdot2.style.opacity = 100;
+    });
+    dot2.addEventListener("mouseout", ()=> {
+        dot2.style.width = "1rem";
+        dot2.style.borderStyle = "none";
+        tdot2.style.opacity = 0;
+    });
+
+    dot3.addEventListener("mouseover", ()=> {
+        dot3.style.width = "1.5rem";
+        dot3.style.borderStyle = "solid";
+        dot3.style.borderColor = "white"
+        tdot3.style.opacity = 100;
+    });
+    dot3.addEventListener("mouseout", ()=> {
+        dot3.style.width = "1rem";
+        dot3.style.borderStyle = "none";
+        tdot3.style.opacity = 0;
+    });
+
+    dot4.addEventListener("mouseover", ()=> {
+        dot4.style.width = "1.5rem";
+        dot4.style.borderStyle = "solid";
+        dot4.style.borderColor = "white"
+        tdot4.style.opacity = 100;
+    });
+    dot4.addEventListener("mouseout", ()=> {
+        dot4.style.width = "1rem";
+        dot4.style.borderStyle = "none";
+        tdot4.style.opacity = 0;
+    });
+
+    dot5.addEventListener("mouseover", ()=> {
+        dot5.style.width = "1.5rem";
+        dot5.style.borderStyle = "solid";
+        dot5.style.borderColor = "white"
+        tdot5.style.opacity = 100;
+    });
+    dot5.addEventListener("mouseout", ()=> {
+        dot5.style.width = "1rem";
+        dot5.style.borderStyle = "none";
+        tdot5.style.opacity = 0;
+    });
 
     js.addEventListener("mouseover", ()=> {
         js.style.color = "#F0DB4F"
